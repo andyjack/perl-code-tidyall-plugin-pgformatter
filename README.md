d145e04f1ff293e9dd14df82b207302c05ece900
Andy Jack <andyjack@cpan.org>
2015-03-31 13:59:24 -0400

    initial commit after running milla new

---
+++ README.md
@@ -0,0 +1,26 @@
+# NAME
+
+Code::TidyAll::Plugin::PgFormatter - Blah blah blah
+
+# SYNOPSIS
+
+    use Code::TidyAll::Plugin::PgFormatter;
+
+# DESCRIPTION
+
+Code::TidyAll::Plugin::PgFormatter is
+
+# AUTHOR
+
+Andy Jack <andyjack@cpan.org>
+
+# COPYRIGHT
+
+Copyright 2015- Andy Jack
+
+# LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# SEE ALSO
---
+++ lib/Code/TidyAll/Plugin/PgFormatter.pm
@@ -0,0 +1,39 @@
+package Code::TidyAll::Plugin::PgFormatter;
+
+use strict;
+use 5.008_005;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Code::TidyAll::Plugin::PgFormatter - Blah blah blah
+
+=head1 SYNOPSIS
+
+  use Code::TidyAll::Plugin::PgFormatter;
+
+=head1 DESCRIPTION
+
+Code::TidyAll::Plugin::PgFormatter is
+
+=head1 AUTHOR
+
+Andy Jack E<lt>andyjack@cpan.orgE<gt>
+
+=head1 COPYRIGHT
+
+Copyright 2015- Andy Jack
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 SEE ALSO
+
+=cut
